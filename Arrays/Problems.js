35d3ae9def6f50eafad44e8ef6e82d5560d41cab
krishna <devenger@krishnas-MacBook-Pro.local>
2021-09-18 20:45:11 +0530

    clean up

--- Arrays/Problems.js
+++ Arrays/Problems.js
@@ -2,13 +2,12 @@ function findLongestConsOne(A) {
   let count = 0;
   let left = [];
   let right = [];
-  // left[0]
   let cnt = 0;
   for (let i = 0; i < A.length; i++) {
     if (A[i] == "1") {
       count++;
       cnt += 1;
-      left.push(cnt); // 1st
+      left.push(cnt); 
     } else {
       left.push(0);
       cnt = 0;
@@ -19,7 +18,7 @@ function findLongestConsOne(A) {
   for (let j = A.length - 1; j >= 0; j--) {
     if (A[j] == "1") {
       rightCount += 1;
-      right.push(rightCount); // 1st
+      right.push(rightCount); 
     } else {
       right.push(0);
       rightCount = 0;
@@ -51,3 +50,22 @@ function findLongestConsOne(A) {
 // findLongestConsOne("1111111111111");
 
 
+
+// To solve this problem we need to understand how B works and 
+// B ~ (B % N) we can replace B with (B % N) this is the key observation
+// 
+
+function rotateArr(A, B) {
+  let N = A.length;
+   B = B % N;
+    let c = []
+    for(let i = 0; i<N; i++) {
+        let newIdx = (B + i) % N;
+        c[newIdx] = A[i]
+    }
+    console.log(c.join(' ') + ' ')
+  // console.log(c + '')
+}
+
+// rotateArr([64, 78, 50, 25, 24, 27, 76, 59, 65, 41, 37, 6, 80, 61, 8, 48, 38, 85, 41, 18, 12, 81, 36, 37, 12, 44, 22, 65, 12, 33, 19, 42, 25, 30, 5, 4, 96, 81, 72, 71, 20, 20, 23, 85, 93, 33, 32, 30, 12, 97, 24, 13, 93, 58, 74, 37, 10, 46, 26, 21, 41, 92, 90, 21, 65, 35, 89, 26, 10, 14, 64, 28, 3, 80, 99, 62, 38, 55, 8, 92, 31, 93, 
+//   58, 77, 21, 34, 57, 49, 82, 85, 38, 89, 66, 51, 4, 31, 12, 33, 83, 29], 13)
